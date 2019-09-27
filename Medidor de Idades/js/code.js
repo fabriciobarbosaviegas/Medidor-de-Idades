69efdf18073061942d500efc4044a2cb7e98f9a9
Fabrício Barbosa Viegas <49844765+fabriciobarbosaviegas@users.noreply.github.com>
2019-09-27 19:58:43 -0300

    Update code.js

--- Medidor de Idades/js/code.js
+++ Medidor de Idades/js/code.js
@@ -1,74 +1,87 @@
 function idade() {
-	var atual = new Date()
-	var turno = atual.getYear()
-	var ano = turno + 1900
-	var sexo = document.getElementsByName('s')
-	var img = document.getElementById('imagen')
-	var n = document.getElementById('n').value
-	var res = document.getElementById('idade')
-	var r = document.getElementById('res')
-	var idade = ano - n
+
+	const data = new Date().getYear() + 1900
+	const sexo = document.getElementsByName('s')
+	const anoNascimento = document.getElementById('n').value
+	const resIdade = document.getElementById('idade')
+	const res = document.getElementById('res')
+	const idade = data - anoNascimento
+	let img = document.getElementById('imagen')
+
 	res.innerHTML = ''
-	r.innerHTML = ''
-	if (n.length == 0) {
+	resIdade.innerHTML = ''
+
+	if (anoNascimento.length == 0) {
 		res.innerHTML = '<p style="color:red">*Adicione sua data de nascimento</p>'
-		r.innerHTML = ''
+		resIdade.innerHTML = ''
 		img.src = 'user.png'
 	}
-	else if (n > ano) {
+
+	else if (anoNascimento > data) {
 		alert('Insira um ano valido')
-		res.innerHTML = `invalido`
+		res.innerHTML = 'invalido'
 		img.src = 'user.png'
 	}
 	
 	else if(idade >= 60){
+
  		if(sexo[0].checked){
 			img.src = 'idoso.jpg'
-			r.innerHTML = `idoso`
-			res.innerHTML = `${idade} anos`
+			res.innerHTML = 'idoso'
+			resIdade.innerHTML = `${idade} anos`
+		}
 
-	}
  		if(sexo[1].checked){
 			img.src = 'idosa.jpg'
-			res.innerHTML = `idosa`
-			res.innerHTML = `${idade} anos`
+			res.innerHTML = 'idosa'
+			resIdade.innerHTML = `${idade} anos`
+		}
 	}
-	 }
+
 	else if (idade < 60 && idade > 18) {
+
  		if(sexo[0].checked){
 			img.src = 'homen.jpg'
-			r.innerHTML = `adulto`
-			res.innerHTML = `${idade} anos`
-	}
+			res.innerHTML = 'adulto'
+			resIdade.innerHTML = `${idade} anos`
+		}
+
  		if(sexo[1].checked){
 			img.src = 'mulher.jpg'
-			res.innerHTML = `adulta`
-			res.innerHTML = `${idade} anos`
-	}			
+			res.innerHTML = 'adulta'
+			resIdade.innerHTML = `${idade} anos`
+		}	
+
 	 }
+
 	else if (idade > 14){
+
  		if(sexo[0].checked){
 			img.src = 'am.png'
-			r.innerHTML = `adolescente`
-			res.innerHTML = `${idade} anos`
-	}
+			res.innerHTML = 'adolescente'
+			resIdade.innerHTML = `${idade} anos`
+		}
+
  		if(sexo[1].checked){
 			img.src = 'af.jpg'
-			res.innerHTML = `adolescente`
-			res.innerHTML = `${idade} anos`
+			res.innerHTML = 'adolescente'
+			resIdade.innerHTML = `${idade} anos`
+		}
 	}
-	}
+
 	else if (idade < 14){
+
  		if(sexo[0].checked){
 			img.src = 'menino.jpg'
-			r.innerHTML = `criança`
-			res.innerHTML = `${idade} anos`
-	}
+			res.innerHTML = 'criança'
+			resIdade.innerHTML = `${idade} anos`
+		}
+
  		if(sexo[1].checked){
 			img.src = 'menina.jpg'
-			res.innerHTML = `criança`
-			res.innerHTML = `${idade} anos`
-	}	
+			res.innerHTML = 'criança'
+			resIdade.innerHTML = `${idade} anos`
+		}	
 	 }
-	
+
 }
